a13920fab11fa6900202ecd880fc5c580880884e
gabe <balcides@hotmail.com>
2019-04-01 23:59:04 -0700

    Lesson 26 - Action Priority
    Cursor actions work with player move and attack. Keeps player from
    hovering over edge of the world.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -10,14 +10,14 @@ namespace RPG.Control
 
         
         void Update(){
-            InteractWithCombat();
-            InteractWithMovement();
-
+            if(InteractWithCombat()) return; //on fail, skips anything after on update
+            if(InteractWithMovement()) return;
         }
 
 
-        private void InteractWithCombat(){
+        private bool InteractWithCombat(){
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
             foreach (RaycastHit hit in hits){
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
                 if(target == null) continue; //if target null, go to the next iteration in the loop
@@ -25,30 +25,24 @@ namespace RPG.Control
                 if(Input.GetMouseButtonDown(0)){
                     GetComponent<Fighter>().Attack(target);
                 }
+                return true;
             }
-
+            return false;
         }
 
 
-        private void InteractWithMovement()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-
-            }
-        }
-
-
-        private void MoveToCursor()
+        private bool InteractWithMovement()
         {
             RaycastHit hit;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
-                GetComponent<Mover>().MoveTo(hit.point);
-
+                if(Input.GetMouseButton(0)){
+                    GetComponent<Mover>().MoveTo(hit.point);
+                }
+                return true;
             }
+            return false;
         }
 
         private static Ray GetMouseRay()
