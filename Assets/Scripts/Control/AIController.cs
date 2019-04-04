01c52f021590ee9da48a418f8942212693253759
gabe <balcides@hotmail.com>
2019-04-03 18:57:07 -0700

    49 - AI Suspicion

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -8,11 +8,14 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
+
         Fighter fighter;
         GameObject player;
         Health health;
         Vector3 guardPosition;
         Mover mover;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start(){
 
@@ -26,12 +29,41 @@ namespace RPG.Control
 
         void Update()
         {
-            if(InAttackRangeOfPlayer() && fighter.CanAttack(player)){
-                fighter.Attack(player);
-
-            }else{
-                mover.StartMoveAction(guardPosition);
+            if(InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                timeSinceLastSawPlayer = 0;
+                AttackBehavior();
             }
+            else if(timeSinceLastSawPlayer < suspicionTime)
+            {
+                //Suspicion state
+                SuspicionBehavior();
+            }
+            else
+            {
+                GuardBehavior();
+            }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+
+        private void GuardBehavior()
+        {
+            //moves AI back to position when chase fails or out of range.
+            mover.StartMoveAction(guardPosition);
+        }
+
+
+        private void SuspicionBehavior()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+
+        private void AttackBehavior()
+        {
+            fighter.Attack(player);
         }
 
 
@@ -40,6 +72,7 @@ namespace RPG.Control
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
         }
+
 
         //Called by Unity
         void OnDrawGizmosSelected()
