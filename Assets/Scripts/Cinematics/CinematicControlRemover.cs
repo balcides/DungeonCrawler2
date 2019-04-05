64ee4755e0cc76d05b5e1a81790cb79e5a10d325
gabe <balcides@hotmail.com>
2019-04-05 02:43:06 -0700

    65-PlayerInputCutscene

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
 
 namespace RPG.Cinematics
 {
     public class CinematicControlRemover : MonoBehaviour
     {
+        GameObject player;
+
         private void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
-
+            player = GameObject.FindWithTag("Player");
         }
+
 
         void DisableControl(PlayableDirector pd){
             print("Disable Control");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+    
         }
+
 
         void EnableControl(PlayableDirector pd){
             print("Enable Control");
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
