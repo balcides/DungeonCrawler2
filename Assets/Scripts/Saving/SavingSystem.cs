588abe4d2c36f6704ac75dfea957ff991a5af56f
gabe <balcides@hotmail.com>
2019-04-19 01:58:45 -0700

    79 - File Paths in C#

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -9,14 +10,18 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            print("Saving to " + saveFile);
+            print("Saving to " + GetPathFromSaveFile(saveFile));
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from " + saveFile);
+            print("Loading from " + GetPathFromSaveFile(saveFile));
         }
 
+        private string GetPathFromSaveFile(string saveFile){
+            
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
+        }
     }
 
 }
