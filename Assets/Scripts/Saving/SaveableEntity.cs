74a7932c48f449471f098e84bcc016bfd58fa7fd
gabe <balcides@hotmail.com>
2019-04-23 19:01:09 -0700

    87 - Serializing with Savable Entities

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -20,25 +20,16 @@ namespace RPG.Saving
 
         public object CaptureState()
         {
-            Dictionary<string, object> state = new Dictionary<string, object>();
-            foreach (ISaveable saveable in GetComponents<ISaveable>())
-            {
-                state[saveable.GetType().ToString()] = saveable.CaptureState();
-            }
-            return state;
+            return new SerializableVector3(transform.position);
         }
 
         public void RestoreState(object state)
         {
-            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
-            foreach (ISaveable saveable in GetComponents<ISaveable>())
-            {
-                string typeString = saveable.GetType().ToString();
-                if (stateDict.ContainsKey(typeString))
-                {
-                    saveable.RestoreState(stateDict[typeString]);
-                }
-            }
+           SerializableVector3 position = (SerializableVector3)state;
+           GetComponent<NavMeshAgent>().enabled = false;
+           transform.position = position.ToVector();
+           GetComponent<NavMeshAgent>().enabled = true;
+           GetComponent<ActionScheduler>().CancelCurrentAction();
         }
 
 #if UNITY_EDITOR
